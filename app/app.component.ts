3a94323c2758b35aef5382a1162212ac261872ba
Esvethlynna Pangelinan and Lincoln Nguyen <esvethlynna+lincoln@>
2017-08-16 11:01:23 -0700

    Add edit button with event bindings to alert keg supplies

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Tap Room for {{month}}/{{day}}/{{year}}</h1>
     <ul>
-      <li><strong>{{firstKeg.name}}</strong> <br> Brand: {{firstKeg.brand}} <br> Price: {{firstKeg.price}} <br> Alcohol Content: {{firstKeg.alcoholContent}}</li>
+      <li (click)="lowSupply(currentKeg)" *ngFor="let currentKeg of kegs"><strong>{{currentKeg.name}}</strong> <br> Brand: {{currentKeg.brand}} <br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}}<br> <button (click)="editKeg()">Edit</button><br><br></li>
     </ul>
   </div>
   `
@@ -17,8 +17,25 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstKeg: Keg = new Keg("Mt. Takhoma Blonde", "German Pilsner Malt", 5.00, "4.4%", 124);
+  kegs: Keg[] = [
+    new Keg("MT. TAKHOMA BLONDE", "German Pilsner Malt", 5.00, "4.4%", 124),
+    new Keg("PINNACLE PEAK PALE ALE", "German malted barley", 7.00, "5.4%", 124),
+    new Keg("PUGET SOUND PORTER", "Columbus and Centennial", 10.00, "8.9%", 124)
+  ];
+
+  editKeg() {
+    alert("You just requested to edit the Keg info!")
   }
+
+  lowSupply(clickedKeg: Keg) {
+    if(clickedKeg.lessThan10Pints === true) {
+      alert("There are less than 10 pints left");
+    } else {
+      alert("There are more than 10 pints for this keg");
+    }
+  }
+
+}
 
 
 
