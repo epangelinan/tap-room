00219612ae9a8846f1ce62c983af67cfdf2133bf
Esvethlynna Pangelinan and Lincoln Nguyen <lincoln@>
2017-08-16 14:51:00 -0700

    add @Input and @Output functionality to keg-list component and update app/root component to include the info from the keg-list component

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Keg } from './keg.model';
   template: `
   <div class="container">
     <h1>Tap Room for {{month}}/{{day}}/{{year}}</h1>
-    <keg-list></keg-list>
+    <keg-list [childKegList]="masterKegList" (clickSender)="editKeg($event)"></keg-list>
     <hr>
     <div>
       <div *ngIf="selectedKeg">
@@ -39,7 +39,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-selectedKeg = null;
+  selectedKeg = null;
+
+  masterKegList: Keg[] = [
+    new Keg("MT. TAKHOMA BLONDE", "German Pilsner Malt", 5.00, "4.4%", 124),
+    new Keg("PINNACLE PEAK PALE ALE", "German malted barley", 7.00, "5.4%", 124),
+    new Keg("PUGET SOUND PORTER", "Columbus and Centennial", 10.00, "8.9%", 124)
+  ];
 
   editKeg(clickedKeg) {
     this.selectedKeg = clickedKeg;
--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -1,21 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Keg } from './keg.model';
 
 @Component({
   selector: 'keg-list',
   template: `
   <ul>
-    <li [class]="priceIndicatorColor(currentKeg)" (click)="lowSupply(currentKeg)" *ngFor="let currentKeg of kegs"><strong>{{currentKeg.name}}</strong> <br> Brand: {{currentKeg.brand}} <br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}}<br> <button (click)="editKeg(currentKeg)">Edit</button><br><br></li>
+    <li [class]="priceIndicatorColor(currentKeg)" (click)="lowSupply(currentKeg)" *ngFor="let currentKeg of childKegList"><strong>{{currentKeg.name}}</strong> <br> Brand: {{currentKeg.brand}} <br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}}<br> <button (click)="editButtonHasBeenClicked(currentKeg)">Edit</button><br><br></li>
   </ul>
   `
 })
 
 export class KegListComponent {
-  kegs: Keg[] = [
-    new Keg("MT. TAKHOMA BLONDE", "German Pilsner Malt", 5.00, "4.4%", 124),
-    new Keg("PINNACLE PEAK PALE ALE", "German malted barley", 7.00, "5.4%", 124),
-    new Keg("PUGET SOUND PORTER", "Columbus and Centennial", 10.00, "8.9%", 124)
-  ];
+  @Input() childKegList: Keg[];
+  @Output() clickSender = new EventEmitter();
 
   lowSupply(clickedKeg: Keg) {
     if(clickedKeg.lessThan10Pints === true) {
@@ -33,5 +30,8 @@ export class KegListComponent {
     }
   }
 
+  editButtonHasBeenClicked(kegToEdit: Keg) {
+    this.clickSender.emit(kegToEdit);
+  }
 
 }
