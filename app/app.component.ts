1e34bef31af1fbf2f25609433efb37d4d1028fb7
Esvethlynna Pangelinan and Lincoln Nguyen <esvethlynna+lincoln@>
2017-08-16 11:53:45 -0700

    add functionality to edit a Keg and display a form

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,8 +6,28 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Tap Room for {{month}}/{{day}}/{{year}}</h1>
     <ul>
-      <li [class]="priceIndicatorColor(currentKeg)" (click)="lowSupply(currentKeg)" *ngFor="let currentKeg of kegs"><strong>{{currentKeg.name}}</strong> <br> Brand: {{currentKeg.brand}} <br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}}<br> <button (click)="editKeg()">Edit</button><br><br></li>
+      <li [class]="priceIndicatorColor(currentKeg)" (click)="lowSupply(currentKeg)" *ngFor="let currentKeg of kegs"><strong>{{currentKeg.name}}</strong> <br> Brand: {{currentKeg.brand}} <br> Price: {{currentKeg.price}} <br> Alcohol Content: {{currentKeg.alcoholContent}}<br> <button (click)="editKeg(currentKeg)">Edit</button><br><br></li>
     </ul>
+    <hr>
+    <div>
+      <h3>{{selectedKeg.name}}</h3>
+      <p>Pints left: {{selectedKeg.pintsLeft}}</p>
+      <h3>Edit Keg</h3>
+      <label>Enter Keg Name:</label>
+      <input [(ngModel)]="selectedKeg.name">
+      <br>
+      <label>Enter Keg Brand:</label>
+      <input [(ngModel)]="selectedKeg.brand">
+      <br>
+      <label>Enter Keg Price:</label>
+      <input [(ngModel)]="selectedKeg.price">
+      <br>
+      <label>Enter Keg Alcohol Content:</label>
+      <input [(ngModel)]="selectedKeg.alcoholContent">
+      <br>
+      <label>Enter Keg Pints:</label>
+      <input [(ngModel)]="selectedKeg.pintsLeft">
+    </div>
   </div>
   `
 })
@@ -22,9 +42,10 @@ export class AppComponent {
     new Keg("PINNACLE PEAK PALE ALE", "German malted barley", 7.00, "5.4%", 124),
     new Keg("PUGET SOUND PORTER", "Columbus and Centennial", 10.00, "8.9%", 124)
   ];
+selectedKeg: Keg = this.kegs[0];
 
-  editKeg() {
-    alert("You just requested to edit the Keg info!")
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
   }
 
   lowSupply(clickedKeg: Keg) {
@@ -42,6 +63,7 @@ export class AppComponent {
       return "bg-info"
     }
   }
+
 
 }
 
